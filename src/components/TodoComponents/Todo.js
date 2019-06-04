05a0d4542ed847a160a7dd4a23a43fa95539e792
Ribthulu <myemail@example.com>
2019-06-04 19:00:49 -0400

    rewrote my todolist and app so that the list was on app.js and  todolist returned the new input; completed mvp; polishing off some of the comments

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,23 +1,8 @@
 /*jshint esversion: 6 */
 import React from 'react';
 
-function List(props) {
-  return (
-    <div className="list-card">
-      <div className="list-info">
-        <h3>{props.list.name}</h3>
-        <p>
-          <strong>task:</strong> {props.list.task}
-        </p>
-        <p>
-          <strong>id:</strong> {props.list.id}
-        </p>
-        <p>
-          <strong>completed</strong> {props.list.completed}
-        </p>
-      </div>
-    </div>
-  );
+function Todo(props) {
+    return <div>{props.todo.task}</div>;
 }
 
-export default List;
+export default Todo;
